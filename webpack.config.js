34c0290a0e297939d9bc4eaf4b52ecd89b10f269
Yong-Zhen Huang <92352655+Brritany@users.noreply.github.com>
2023-09-28 02:31:52 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,5 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
   }
 };
