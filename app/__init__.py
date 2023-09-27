bedb56287389f86f41e6a887e759537aabdd6c69
Yong-Zhen Huang <92352655+Brritany@users.noreply.github.com>
2023-09-28 02:13:04 +0800

    add

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+import serverless_wsgi
 
 app = Flask(__name__)
 
 from app import routes
+
+def handler(event, context):
+    return serverless_wsgi.handle_request(app, event, context)
