6feb7457c47610377354283dad492525167913ac
Yong-Zhen Huang <92352655+Brritany@users.noreply.github.com>
2023-10-01 01:54:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-// Initialize Jupyter environment here
+window.onload = function() {
+  document.getElementById("editor").contentEditable = true;
+};
 
 function submitCode() {
-  // Run user code and validate
+  const userCode = document.getElementById("editor").innerText;
+
+  // Simulate code execution
+  if (userCode.trim() === "import pandas as pd") {
+    document.getElementById("output").innerText = "Challenge passed!";
+  } else {
+    document.getElementById("output").innerText = "Challenge failed!";
+  }
 }
