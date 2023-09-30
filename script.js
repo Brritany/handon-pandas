9907efc301304997baf6afaeb27119884f0c759f
Yong-Zhen Huang <92352655+Brritany@users.noreply.github.com>
2023-10-01 01:57:26 +0800

    add

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.onload = function() {
 
 function submitCode() {
   const userCode = document.getElementById("editor").innerText;
-
+  
   // Simulate code execution
   if (userCode.trim() === "import pandas as pd") {
     document.getElementById("output").innerText = "Challenge passed!";
