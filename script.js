c08b86da0d3d2fbfc36635955ed1291c9c5a12be
Yong-Zhen Huang <92352655+Brritany@users.noreply.github.com>
2023-10-01 02:25:21 +0800

    add

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-window.onload = function() {
-  document.getElementById("editor").contentEditable = true;
-};
+/* ...existing JS ... */
 
-function submitCode() {
-  const userCode = document.getElementById("editor").innerText;
-  
-  // Simulate code execution for the specific challenge
-  if (userCode.includes("pd.DataFrame") && userCode.includes("'Name'") && userCode.includes("'Age'")) {
-    document.getElementById("output").innerText = "Output: Challenge passed!";
-    document.getElementById("answer").innerText = "Answer: You successfully created a DataFrame!";
+function toggleAnswer() {
+  const answerElement = document.getElementById("answer");
+  if (answerElement.style.display === "none") {
+    answerElement.style.display = "block";
   } else {
-    document.getElementById("output").innerText = "Output: Challenge failed!";
-    document.getElementById("answer").innerText = "Answer: Please follow the scenario and hints to complete the challenge.";
+    answerElement.style.display = "none";
   }
 }
