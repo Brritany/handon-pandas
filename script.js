5929ebdcf0ddbb8cf38b7151232489941017cb89
Yong-Zhen Huang <92352655+Brritany@users.noreply.github.com>
2023-10-01 02:46:34 +0800

    add

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Updated JS for final functionality */
+/* Updated JS to display the actual program output and expected output */
 function toggleAnswer() {
   const answerElement = document.getElementById('answer');
   if (answerElement.style.display === 'none') {
@@ -10,10 +10,15 @@ function toggleAnswer() {
 
 function submitCode() {
   const userCode = document.getElementById('editor').value;
-  const outputElement = document.getElementById('output');
+  const programOutputElement = document.getElementById('program-output');
+  const resultElement = document.getElementById('result');
+
+  // Simulate the actual output based on the code (this is a simplified example and doesn't run the code)
   if (userCode.includes('pd.DataFrame') && userCode.includes("'Name'") && userCode.includes("'Age'")) {
-    outputElement.innerHTML = 'Challenge passed!';
+    programOutputElement.innerHTML = "DataFrame with columns 'Name' and 'Age'";
+    resultElement.innerHTML = 'Challenge passed!';
   } else {
-    outputElement.innerHTML = 'Challenge failed!';
+    programOutputElement.innerHTML = 'Invalid DataFrame';
+    resultElement.innerHTML = 'Challenge failed!';
   }
 }
