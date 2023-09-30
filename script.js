5267c0982e7e5e520e0261e0a72e52d189e382c8
Yong-Zhen Huang <92352655+Brritany@users.noreply.github.com>
2023-10-01 02:34:31 +0800

    add

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-/* ...existing JS ... */
-
+/* Updated JS for final functionality */
 function toggleAnswer() {
-  const answerElement = document.getElementById("answer");
-  if (answerElement.style.display === "none") {
-    answerElement.style.display = "block";
+  const answerElement = document.getElementById('answer');
+  if (answerElement.style.display === 'none') {
+    answerElement.style.display = 'block';
   } else {
-    answerElement.style.display = "none";
+    answerElement.style.display = 'none';
+  }
+}
+
+function submitCode() {
+  const userCode = document.getElementById('editor').value;
+  const outputElement = document.getElementById('output');
+  if (userCode.includes('pd.DataFrame') && userCode.includes("'Name'") && userCode.includes("'Age'")) {
+    outputElement.innerHTML = 'Challenge passed!';
+  } else {
+    outputElement.innerHTML = 'Challenge failed!';
   }
 }
