f47ac5eab9d1c437e373b40a73328f41a3b62aa1
Yong-Zhen Huang <92352655+Brritany@users.noreply.github.com>
2023-10-01 02:20:47 +0800

    add

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ window.onload = function() {
 function submitCode() {
   const userCode = document.getElementById("editor").innerText;
   
-  // Simulate code execution
-  if (userCode.trim() === "import pandas as pd") {
-    document.getElementById("output").innerText = "Challenge passed!";
+  // Simulate code execution for the specific challenge
+  if (userCode.includes("pd.DataFrame") && userCode.includes("'Name'") && userCode.includes("'Age'")) {
+    document.getElementById("output").innerText = "Output: Challenge passed!";
+    document.getElementById("answer").innerText = "Answer: You successfully created a DataFrame!";
   } else {
-    document.getElementById("output").innerText = "Challenge failed!";
+    document.getElementById("output").innerText = "Output: Challenge failed!";
+    document.getElementById("answer").innerText = "Answer: Please follow the scenario and hints to complete the challenge.";
   }
 }
